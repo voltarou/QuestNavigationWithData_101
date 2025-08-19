24740a998d644bd4a2ef74b3f78116c7f2181ade
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-19 15:39:25 +0700

    Perbaikan error 2

--- app/src/main/java/com/example/act7test/view/Formulir.kt
+++ app/src/main/java/com/example/act7test/view/Formulir.kt
@@ -19,7 +19,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
