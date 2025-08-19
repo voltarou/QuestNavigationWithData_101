4cb960341473aa4dd1a3c9a295c7e098ceb1ef58
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-19 17:26:18 +0700

    main

--- app/src/main/java/com/example/act7test/MainActivity.kt
+++ app/src/main/java/com/example/act7test/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.example.act7test
 
+import DataApp
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.act7test.ui.theme.Act7TestTheme
-import com.example.act7test.uicontroller.DataApp
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
