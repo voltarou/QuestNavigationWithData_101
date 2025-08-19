51ef40fc040f3385b8d2764441fe5b4f16dddd54
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-19 16:16:52 +0700

    Edit nav

--- app/src/main/java/com/example/act7test/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/act7test/uicontroller/Navigasi.kt
@@ -3,13 +3,16 @@ package com.example.act7test.uicontroller
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.act7test.view.TampilData
 import com.example.act7test.view.FormIsian
+import com.example.act7test.viewmodel.SiswaViewModel
 
 enum class Navigasi {
     Formulir,
@@ -18,9 +21,11 @@ enum class Navigasi {
 
 @Composable
 fun DataApp(
+    viewModel: SiswaViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ) {
     Scaffold { paddingValues ->
+        val uiState = viewModel.statusUI.collectAsState()
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulir.name,
