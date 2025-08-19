b8ad222e5ebfd4dd3dea227a69f6ab5ab479fa30
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-19 16:16:40 +0700

    Perbaikan error 5

--- app/src/main/java/com/example/act7test/view/Formulir.kt
+++ app/src/main/java/com/example/act7test/view/Formulir.kt
@@ -2,14 +2,17 @@ package com.example.act7test.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
@@ -20,58 +23,72 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.act7test.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FormIsian(pilihanJK: List<String>, onSubmitBtnClick: () -> Unit, it: String) {
-    var txtNama by rememberSaveable { mutableStateOf("") }
+fun IsiRuang(
+    onSubmitButtonClicked: (ArrayList<String>) -> Unit
+) {
+    var txtNama by remember { mutableStateOf("") }
     var txtAlamat by remember { mutableStateOf("") }
     var txtGender by remember { mutableStateOf("") }
-    val listData: MutableList<String> = mutableListOf(txtNama,txtGender,txtAlamat)
+
+    val pilihanJK = listOf("Laki-laki", "Perempuan")
+    val listData = arrayListOf<String>(txtNama, txtGender, txtAlamat)
 
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = stringResource(id = R.string.tampilan_data), color = Color.White) },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.teal_700))
+                title = { Text(text = stringResource(id = R.string.form_pendaftaran), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.purple_500))
             )
         }
-    ) { isiRuang ->
+    ) { paddingValues ->
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+                .padding(paddingValues)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             OutlinedTextField(
                 value = txtNama,
-                onValueChange = { txtNama = it },
                 singleLine = true,
-                modifier = Modifier.fillMaxWidth(),
-                label = { Text(text = "Nama Lengkap") }
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.width(250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = { txtNama = it }
             )
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                verticalAlignment = Alignment.CenterVertically
+            Divider(
+                thickness = 1.dp,
+                modifier = Modifier
+                    .padding(all = 12.dp)
+                    .width(250.dp)
+            )
+
+            // Gender selection
+            Column(
+                modifier = Modifier.width(250.dp)
             ) {
-                Text("Jenis Kelamin:", modifier = Modifier.weight(1f))
+                Text(text = "Jenis Kelamin")
                 pilihanJK.forEach { item ->
                     Row(
-
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.weight(1f)
+                        modifier = Modifier
+                            .selectable(
+                                selected = txtGender == item,
+                                onClick = { txtGender = item }
+                            ),
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
                         RadioButton(
                             selected = txtGender == item,
@@ -82,39 +99,39 @@ fun FormIsian(pilihanJK: List<String>, onSubmitBtnClick: () -> Unit, it: String)
                 }
             }
 
+            Divider(
+                thickness = 1.dp,
+                modifier = Modifier
+                    .padding(all = 5.dp)
+                    .width(250.dp)
+            )
+
             OutlinedTextField(
                 value = txtAlamat,
-                onValueChange = { txtAlamat = it },
                 singleLine = true,
-                modifier = Modifier.fillMaxWidth(),
-                label = { Text(text = "Alamat") }
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.width(250.dp),
+                label = { Text(text = "Alamat Lengkap") },
+                onValueChange = { txtAlamat = it }
             )
 
             Spacer(modifier = Modifier.height(20.dp))
 
             Button(
-                onClick = {onSubmitBtnClick(listData)},
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(fraction = 0.8f),
+                enabled = txtNama.isNotEmpty() && txtAlamat.isNotEmpty() && txtGender.isNotEmpty(),
+                onClick = { onSubmitButtonClicked(listData) }
             ) {
-                Text(text = stringResource(id = R.string.submit))
+                Text(text = stringResource(R.string.submit))
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun IsiRuang(
-    paddingValues: PaddingValues,
-    txtNama: String,
-    txtAlamat: String,
-    onNamaLengkapChange: (String) -> Unit,
-    onAlamatChange: (String) -> Unit,
-    jenisK: List<String>,
-    jenisKelamin: String,
-    onJenisKelaminSelected: (String) -> Unit,
-    onSubmitBtnClick: () -> Unit,
-    listData: MutableList<String> = mutableListOf(txtNama,jenisKelamin,txtAlamat)
-
-) {
-
+fun PreviewIsiRuang() {
+    IsiRuang(
+        onSubmitButtonClicked = {}
+    )
 }
